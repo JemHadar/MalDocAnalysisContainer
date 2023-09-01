4a5b5e7a4f16804859177e7405e161ad4fa34619
Knight Slayer <58823454+JemHadar@users.noreply.github.com>
2023-09-01 12:34:35 -0500

    updated

--- mal_doc_container.sh
+++ mal_doc_container.sh
@@ -7,6 +7,7 @@ then
   exit 1
 fi
 
+# Name the image and container
 image_name="malicious-documents"
 container_name="${image_name}_$(date +%s)"
 
@@ -15,7 +16,7 @@ if [[ "$(docker images -q $image_name 2> /dev/null)" == "" ]]; then
   docker build -t $image_name .
 fi
 
-# Create a temporary tar file and add maldoc specified in the argument of the script
+# Create a temporary tar file and add maldoc specified in the argument of the script via @
 tar_file="$(mktemp)"
 tar -cf "$tar_file" "$@"
 
@@ -27,11 +28,12 @@ docker cp "$tar_file" "$container_name:/home/maldoc/files.tar"
 docker exec "$container_name" tar -xf "/home/maldoc/files.tar" -C "/home/maldoc"
 docker exec "$container_name" rm "/home/maldoc/files.tar"
 
+# Running the container to an interactive shell
 docker exec -it $container_name /bin/bash
 
-# After the Docker container has finished running, kill and remove it
+# When exiting the container, the script will kill and remove it
 docker kill $container_name
 docker rm $container_name
 
-# Remove the temporary tar file
+# Remove created tar temp file
 rm "$tar_file"
